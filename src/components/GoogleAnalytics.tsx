6786019474a532e364854d2b18b643cea1b88aa3
mhmdshameer <mhmdshameerpm@gmail.com>
2025-12-20 14:11:59 +0530

    Google tag update

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+
+declare global {
+    interface Window {
+        dataLayer: any[];
+        gtag: (...args: any[]) => void;
+    }
+}
 
 const GoogleAnalytics = () => {
     const location = useLocation();
     const isAdminRoute = location.pathname.startsWith("/admin") || location.pathname.startsWith("/signin");
+
+    useEffect(() => {
+        if (!isAdminRoute && window.gtag) {
+            window.gtag("config", "G-WK213FF0FJ", {
+                page_path: location.pathname + location.search,
+            });
+        }
+    }, [location, isAdminRoute]);
 
     if (isAdminRoute) {
         return null;
@@ -11,14 +27,12 @@ const GoogleAnalytics = () => {
 
     return (
         <Helmet>
-            {/* Google tag (gtag.js) */}
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WK213FF0FJ"></script>
-            <script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WK213FF0FJ" />
+            <script id="google-analytics">
                 {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', 'G-WK213FF0FJ');
         `}
             </script>
